54ac09a973afde70796570f92ac7fe35fee3b64f
Bereket_Tilahun <btilahun287@gmail.com>
2022-03-08 17:42:26 +0300

    Easy

--- Easy/1137. N-th Tribonacci Number.py
+++ Easy/1137. N-th Tribonacci Number.py
@@ -1,6 +1,6 @@
 # Time Complexity O(n)
 # Space Complexity O(1)
-
+# dp soluiton finding the right answer
 class Solution:
     def tribonacci(self, n: int) -> int:
         if n==0:
@@ -9,7 +9,8 @@ class Solution:
             return 1
         num1=0
         num2=1
-        num3=1
+        num3=1     
+        
         for i in range(2,n):
             num=num1+num2+num3
             num3,num2,num1=num,num3,num2
